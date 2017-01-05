151abe404a4bfa3f20151016827734a2bba24b7c
kapsterz <wegodeboy@yandex.com>
2017-01-05 14:36:10 +0200

    Added tamplates table, first step to done :)

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class RegisterForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
--- blog/views.py
+++ blog/views.py
@@ -37,3 +37,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def register(request):
+    return render(request, 'blog/register.html')
